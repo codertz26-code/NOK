b64da125f887802f4599d5838e8aa84ba34ef561
codertz26-code <codertz26@gmail.com>
2026-04-08 16:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const {
   jidDecode,
   fetchLatestBaileysVersion,
   Browsers
-} = require('baileys');
+} = require('@whiskeysockets/baileys');
 
 const { Boom } = require("@hapi/boom");
 const pino = require("pino");
